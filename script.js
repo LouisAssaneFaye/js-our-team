9a86baf7d36706b733218524999fe55cb0267b24
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-05-29 14:41:36 +0200

    Milestone 0 completato

---
+++ script.js
@@ -0,0 +1,47 @@
+const array = [
+    
+    {
+        nome: " Wayne " ,
+        cognome: " Barnett " ,
+        ruolo: " Founder & CEO " ,
+
+    },
+
+    {
+        nome: " Angela " ,
+        cognome: " Caroll" ,
+        ruolo: " Chief Editor " ,
+
+    },
+
+    {
+        nome: " Walter " ,
+        cognome: "Gordon " ,
+        ruolo: " Official manager " ,
+
+    },
+
+    {
+        nome: " Angela" ,
+        cognome: " Lopez" ,
+        ruolo: "Social media manager" ,
+
+    },
+
+    {
+        nome: " Scott " ,
+        cognome: " Estrada " ,
+        ruolo: " Developer " ,
+
+    },
+
+    {
+        nome: " Barbara " ,
+        cognome: " Ramos" ,
+        ruolo: " Graphic designer " ,
+
+    }
+
+]
+
+console.log(array);
