b13704838f1a99fcaa1695916f5c8cd64ecd110a
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-05-30 16:08:12 +0200

    Correzione errore nell'esercizio

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const array = [
     {
         nome: " Angela Lopez " ,
         ruolo: " Social media manager " ,
-        foto: " "
+        foto: " angela-lopez-social-media-manager.jpg "
 
     },
 
@@ -47,7 +47,7 @@ const array = [
 console.log(array);
 const p = document.querySelector('p');
 
-for (let i = 0 ; i < array.length - 1 ; i = i + 1){
+for (let i = 0 ; i < array.length ; i = i + 1){
     const listainfo = array[i];
     for (let key in listainfo){
         console.log(key , listainfo[key]);
